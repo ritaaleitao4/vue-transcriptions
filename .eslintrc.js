86050befe36d4962410de35f4bb1ab09f5951a6e
Rita Leitão <rita.leitao@hypnotic.pt>
2020-01-31 10:31:07 +0000

    add files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/airbnb'
+  ],
+  rules: {
+    'indent': 'off',
+    'max-len': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    semi: [1, 'never'],
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
