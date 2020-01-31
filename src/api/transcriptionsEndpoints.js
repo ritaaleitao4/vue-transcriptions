e992a0394146ca70bc7001e86bebedfeb0d8bd54
Rita Leitão <rita.leitao@hypnotic.pt>
2020-01-31 16:42:16 +0000

    https

--- src/api/transcriptionsEndpoints.js
+++ src/api/transcriptionsEndpoints.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 export default {
   getTranscriptions() {
-    return axios.get('http://www.mocky.io/v2/5ae1c5792d00004d009d7e5c')
+    return axios.get('https://www.mocky.io/v2/5ae1c5792d00004d009d7e5c')
   },
   saveTranscriptions(payload) {
-    return axios.post('http://www.mocky.io/v2/5ae1c5792d00004d009d7e5c', payload)
+    return axios.post('https://www.mocky.io/v2/5ae1c5792d00004d009d7e5c', payload)
   },
 }
